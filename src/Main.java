60c7f2518aa0023926b7dd31c3ccc3c32e55dd07
Yamuna Kumari <yamunakumari961@gmail.com>
2022-10-19 17:25:14 +0530

    Added comment to make code readable.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int arr[] = new int[]{2, 5, 1, 0, 4, 8};
+        int[] arr = new int[]{2, 5, 1,-12 , 0, 4, 8};
         SortingAlgorithm sortingAlgorithm = new SortingAlgorithm(arr);
         //sorted using bubble sort algo.
         sortingAlgorithm.bubbleSort();
--- src/SortingAlgorithm.java
+++ src/SortingAlgorithm.java
@@ -1,28 +1,34 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SortingAlgorithm {
-    private int arr[];
+    private final int[] arr;
+    int count = 0;
 
-    public SortingAlgorithm(int arr[]) {
+    public SortingAlgorithm(int[] arr) {
         this.arr = arr;
     }
 
     public void bubbleSort() {
+        int pass = 0;
+        System.out.println("Original Array : " + Arrays.toString((arr)));
+        System.out.println();
+
         //Iterate a loop by array item size
         for (int index = 0; index < arr.length; index++) {
             //iterate over the array for pair of item
-            for (int element = 0; element < arr.length - 1; element++)
+            pass++;
+            for (int i = 0; i < arr.length - 1; i++)
                 //compare each pair and swap if condition true
-                if (arr[element] > arr[element + 1]) {
+                if (arr[i] > arr[i + 1]) {
                     //swapping element in array
-                    int temp = arr[element];
-                    arr[element] = arr[element + 1];
-                    arr[element + 1] = temp;
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
                 }
+            count++;
+            System.out.println(pass + " pass result : " + Arrays.toString((arr)));
         }
-
-        System.out.println("Sorted Array : " + Arrays.toString((arr)));
+        System.out.println("\n" + "Sorted Array : " + count + " times  :  " + Arrays.toString((arr)));
     }
 
 }
