6f818e7ee94af4901f0f3dc8ac2c096b39691133
Yamuna Kumari <yamunakumari961@gmail.com>
2022-10-19 14:15:20 +0530

    implemented bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int arr[] = new int[]{2, 5, 1, 0, 4, 8};
+        SortingAlgorithm sortingAlgorithm = new SortingAlgorithm(arr);
+        //sorted using bubble sort algo.
+        sortingAlgorithm.bubbleSort();
+
     }
 }
--- src/SortingAlgorithm.java
+++ src/SortingAlgorithm.java
@@ -1,23 +1,28 @@
-public class BubbleSort {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class SortingAlgorithm {
     private int arr[];
 
-    public BubbleSort(int arr[]) {
+    public SortingAlgorithm(int arr[]) {
         this.arr = arr;
-
     }
 
-    public int[] sort() {
-        for (int i = 0; i < arr.length; i++) {
-            for(int j=0; j<arr.length-1; j++)
-            if (arr[j] > arr[j + 1]) {
-                int temp= arr[j];
-                arr[j]= arr[j+1];
-                arr[j+1]= temp;
-
-            }
+    public void bubbleSort() {
+        //Iterate a loop by array item size
+        for (int index = 0; index < arr.length; index++) {
+            //iterate over the array for pair of item
+            for (int element = 0; element < arr.length - 1; element++)
+                //compare each pair and swap if condition true
+                if (arr[element] > arr[element + 1]) {
+                    //swapping element in array
+                    int temp = arr[element];
+                    arr[element] = arr[element + 1];
+                    arr[element + 1] = temp;
+                }
         }
 
-        return arr;
+        System.out.println("Sorted Array : " + Arrays.toString((arr)));
     }
 
 }
